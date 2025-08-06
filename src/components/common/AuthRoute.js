4a3f974e0ed1946bd18348a5056988d441e7ff3a
unknown <everythingfree4200@gmail.com>
2025-08-06 09:19:54 +0530

    009

---
+++ src/components/common/AuthRoute.js
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import AuthContext from '../../context/AuthContext';
+
+const AuthRoute = () => {
+  const { isAuthenticated, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    </div>;
+  }
+
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/" replace />;
+};
+
+export default AuthRoute;
